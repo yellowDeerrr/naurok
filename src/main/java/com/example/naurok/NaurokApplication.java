1b69ef712d7e4179ea3924e98a7232f525ab3c1f
yelowDeerrr <emailforgithubbanksystem@gmail.com>
2024-02-25 15:52:29 +0200

    first commit

---
+++ src/main/java/com/example/naurok/NaurokApplication.java
@@ -0,0 +1,13 @@
+package com.example.naurok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaurokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaurokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/naurok/NaurokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.naurok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaurokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
